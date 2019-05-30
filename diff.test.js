76f6e89b5a61f86e0054595b079e2b501944ce7c
Haishan <haishanhan@gmail.com>
2019-05-30 17:42:51 +0800

    test: add more array related cases for diff

--- diff.test.js
+++ diff.test.js
@@ -2,6 +2,8 @@
 
 const diff = require('./diff');
 
+// diff(stateNext, statePrev)
+
 test('one', () => {
   const o1 = {
     a: 3,
@@ -83,4 +85,59 @@ test('one4', () => {
   };
   const ret = diff(o1, o2);
   expect({}).toEqual(ret);
+});
+
+test('arr0', () => {
+  const obj0 = {};
+  const obj1 = {};
+
+  const o1 = {
+    a: [obj0, [obj0, obj1, 'change']],
+    b: [obj0, obj1]
+  };
+  const o2 = {
+    a: [obj0, [obj0, obj1, 'o']],
+    b: [obj0, obj1]
+  };
+  const ret = diff(o1, o2);
+  expect({ 'a[1][2]': 'change' }).toEqual(ret);
+});
+
+test('arr1', () => {
+  const obj0 = {};
+  const obj1 = {};
+
+  const o2 = {
+    a: [obj0, [obj0, obj1, 'o']],
+    b: [obj0, obj1]
+  };
+
+  // since we are diff with referential equity
+  // this should not result out a path change
+  obj1.a = 'a';
+
+  const o1 = {
+    a: [obj0, [obj0, obj1, 'change']],
+    b: [obj0, obj1]
+  };
+  const ret = diff(o1, o2);
+  expect({ 'a[1][2]': 'change' }).toEqual(ret);
+});
+
+test('arr2', () => {
+  const obj0 = {};
+  const obj1 = {};
+
+  const o2 = {
+    a: [obj0, [obj0, obj1, 'o']],
+    b: [obj0, obj1]
+  };
+
+  const o1 = {
+    a: [obj0, [obj0, obj1, 'change']],
+    b: [obj0, { a: 'a' }]
+  };
+
+  const ret = diff(o1, o2);
+  expect({ 'a[1][2]': 'change', 'b[1].a': 'a' }).toEqual(ret);
 });
